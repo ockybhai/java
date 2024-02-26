5ac5e61bebc804f59758546851094e2058ac5761
ockybhai <146436540+ockybhai@users.noreply.github.com>
2024-02-26 12:14:50 +0530

    Add files via upload

---
+++ sort.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class sort {
+public static void main(String[] args) {
+System.out.println("DENNY T DILEEP \n 23MCA025\n 26-FEB-2024");
+String[] strings = {"banana", "apple", "cherry", "watermelon"};
+System.out.println("Original array: " + Arrays.toString(strings));
+Arrays.sort(strings);
+System.out.println("Sorted array: " + Arrays.toString(strings));
+}
+}
